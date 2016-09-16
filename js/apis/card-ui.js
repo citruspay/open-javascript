6d707536c9980f03778e973173a35b095793632f
Rakesh Pathak <rakesh@talentica-all.com>
2016-09-16 13:50:37 +0530

    added default styles to iframe and input for hosted fields. if invalid field type is being sent we will throw an error.

--- js/apis/card-ui.js
+++ js/apis/card-ui.js
@@ -9,20 +9,44 @@ console.log(document.location.href);
     paymentField = document.createElement("input");
     paymentField.setAttribute("id", field[1]+"citrusInput");
     document.body.appendChild(paymentField);
-
-    if (window.addEventListener) {
-        paymentField.addEventListener("blur", focusOutFunction, false)
-    } else {
-        paymentField.attachEvent("blur", focusOutFunction)
+    var placeHolder = "";
+    switch(field[1]){
+        case "cvv":
+            placeHolder="cvv";
+            break;
+        case "number":
+            placeHolder="card number";
+            break;
+        case "expiry":
+            placeHolder = "expiry(mm/yy)"
+            break;
     }
-    //element.addEventListener("focusout", focusOutFunction);
+    var defaultStyle ={
+        background: 0,
+        display: 'inline-block',
+        width: '78%',
+        padding: "10px 0",
+        fontSize: '13px'
+    };
+    paymentField.setAttribute('placeholder',placeHolder);
+    Object.assign(paymentField.style,defaultStyle);
+    if (window.addEventListener) {
+        paymentField.addEventListener("blur", postPaymentData, false);
+    } else {
+        paymentField.attachEvent("blur", postPaymentData);
+    }
 };
 
-const focusOutFunction = () => {
+const postPaymentData = () => {
     //Send value of the field to the cardnumber iframe
    let cardData = {};
     cardData[field[1]] = paymentField.value;
+    //todo:IMPORTANT, change * to citrus server url,
+    //also if possible use name instead of index as index will be unreliable
+    //if there are other iframes on merchant's page
+    parent.window.frames[0].postMessage(cardData, "*");
     parent.window.frames[1].postMessage(cardData, "*");
+    parent.window.frames[2].postMessage(cardData, "*");
 };
 
 export {cardFieldHandler}
