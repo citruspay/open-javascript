06c03b118bf653e57b51248a49439c7c1876129a
nagamaCitruspay <nagama.inamdar@citruspay.com>
2016-09-30 16:57:15 +0530

    handled the test scenario for single tab for both credit/debit card

--- js/apis/payment.js
+++ js/apis/payment.js
@@ -2,7 +2,7 @@ import {baseSchema} from "./../validation/validation-schema";
 import cloneDeep from "lodash/cloneDeep";
 import {urlReEx} from "../constants";
 import {handlersMap, getConfig} from "../config";
-import {getAppData, setAppData, isIE} from './../utils';
+import {getAppData, setAppData, isIE} from "./../utils";
 import {singleHopDropOutFunction, singleHopDropInFunction} from "./singleHop";
 import {refineMotoResponse} from "./response";
 import {custFetch} from "../interceptor";
@@ -27,99 +27,100 @@ const motoCardValidationSchema = Object.assign(cloneDeep(baseSchema), {
 
 motoCardValidationSchema.mainObjectCheck.keysCheck.push('paymentDetails');
 const makePayment = (paymentObj) => {
-
     txnId = paymentObj.merchantTxnId;
     const paymentMode = paymentObj.paymentDetails.paymentMode.toLowerCase().replace(/\s+/g, '');
-    const win = document.getElementById("citrusnumber-" + paymentObj.paymentDetails.type.toLowerCase()).contentWindow;
+    let element = document.getElementById("citrusnumber-" + paymentObj.paymentDetails.type.toLowerCase());
+    if (typeof(element) === 'undefined' || element === null)
+        element = document.getElementById("citrusnumber-card");
+    const win = element.contentWindow;
     paymentObj.pgSettingsData = getAppData('pgSettingsData');
     paymentObj.config = getConfig();
 
-    if (paymentObj.mode.toLowerCase() !== "dropout")
-        {
-            // winRef = openPopupWindow("");
-            // winRef.document.write('<html><head> <meta name="viewport" content="width=device-width"/> <meta http-equiv="Cache-control" content="public"/> <title>Redirecting to Bank</title></head><style>body{background: #fafafa;}#wrapper{position: fixed; position: absolute; top: 10%; left: 0; right: 0; margin: 0 auto; font-family: Tahoma, Geneva, sans-serif; color: #000; text-align: center; font-size: 14px; padding: 20px; max-width: 500px; width: 70%;}.maintext{font-family: Roboto, Tahoma, Geneva, sans-serif; color: #f6931e; margin-bottom: 0; text-align: center; font-size: 16pt; font-weight: 400;}.textRedirect{color: #675f58;}.subtext{margin: 15px 0 15px; font-family: Roboto, Tahoma, Geneva, sans-serif; color: #929292; text-align: center; font-size: 10pt;}.subtextOne{margin: 35px 0 15px; font-family: Roboto, Tahoma, Geneva, sans-serif; color: #929292; text-align: center; font-size: 10pt;}@media screen and (max-width: 480px){#wrapper{max-width: 100%!important;}}</style><body> <div id="wrapper"> <div id="imgtext" style="margin-left:1%; margin-bottom: 5px;"><!--<img src="https://context.citruspay.com/kiwi/images/logo.png"/>--> </div><div id="imgtext" style="text-align:center;padding: 15% 0 10%;"><!---<img src="https://context.citruspay.com/kiwi/images/puff_orange.svg"/>--></div><p class="maintext">Processing <span class="textRedirect">Payment</span> </p><p class="subtext"><span>We are redirecting you to the bank\'s page</span></p><p class="subtextOne"><span>DO NOT CLOSE THIS POP-UP</span> </p></div></body></html>');
-        }
-            setAppData('paymentObj', paymentObj);
+    if (paymentObj.mode.toLowerCase() !== "dropout") {
+        // winRef = openPopupWindow("");
+        // winRef.document.write('<html><head> <meta name="viewport" content="width=device-width"/> <meta http-equiv="Cache-control" content="public"/> <title>Redirecting to Bank</title></head><style>body{background: #fafafa;}#wrapper{position: fixed; position: absolute; top: 10%; left: 0; right: 0; margin: 0 auto; font-family: Tahoma, Geneva, sans-serif; color: #000; text-align: center; font-size: 14px; padding: 20px; max-width: 500px; width: 70%;}.maintext{font-family: Roboto, Tahoma, Geneva, sans-serif; color: #f6931e; margin-bottom: 0; text-align: center; font-size: 16pt; font-weight: 400;}.textRedirect{color: #675f58;}.subtext{margin: 15px 0 15px; font-family: Roboto, Tahoma, Geneva, sans-serif; color: #929292; text-align: center; font-size: 10pt;}.subtextOne{margin: 35px 0 15px; font-family: Roboto, Tahoma, Geneva, sans-serif; color: #929292; text-align: center; font-size: 10pt;}@media screen and (max-width: 480px){#wrapper{max-width: 100%!important;}}</style><body> <div id="wrapper"> <div id="imgtext" style="margin-left:1%; margin-bottom: 5px;"><!--<img src="https://context.citruspay.com/kiwi/images/logo.png"/>--> </div><div id="imgtext" style="text-align:center;padding: 15% 0 10%;"><!---<img src="https://context.citruspay.com/kiwi/images/puff_orange.svg"/>--></div><p class="maintext">Processing <span class="textRedirect">Payment</span> </p><p class="subtext"><span>We are redirecting you to the bank\'s page</span></p><p class="subtextOne"><span>DO NOT CLOSE THIS POP-UP</span> </p></div></body></html>');
+    }
+    setAppData('paymentObj', paymentObj);
     //todo : change the star value to citruspay domain string
-            win.postMessage(paymentObj, "*");
-    };
+    win.postMessage(paymentObj, "*");
+};
 
 const listener = (event) => {
-    try{
-     if(!event.data.isValidRequest) {
-        const motoResponse = event.data;
-        const paymentObj = getAppData('paymentObj');
-        if (event.origin === ("http://localhost") && motoResponse.redirectUrl) { //url check has to configured, currently its hardcoded
-            if (paymentObj.mode.toLowerCase() === "dropout") {
-                singleHopDropOutFunction(motoResponse.redirectUrl);
-            }
-            else {
-                // if(winRef && winRef.closed)
-                // {
-                //     //handlersMap["serverErrorHandler"](response);
-                //     return;
-                // }
-                singleHopDropInFunction(motoResponse.redirectUrl).then(function (response) {
-                    if (winRef && winRef.closed !== true) {
-                        let el = document.createElement('body');
-                        el.innerHTML = response;
-                        let form = el.getElementsByTagName('form');
-                        try {
-                            let paymentForm = document.createElement('form');
-                            switch (Object.prototype.toString.call(form)) {
-                                case "[object NodeList]" :
-                                    paymentForm.setAttribute("action", form[0].action),
-                                        paymentForm.setAttribute("method", form[0].method),
-                                        paymentForm.setAttribute("target", winRef.name),
-                                        paymentForm.innerHTML = form[0].innerHTML,
-                                        document.documentElement.appendChild(paymentForm),
-                                        paymentForm.submit(),
-                                        document.documentElement.removeChild(paymentForm);
-                                    break;
-                                case "[object HTMLCollection]" :
-                                    paymentForm.setAttribute("action", form.submitForm.action),
-                                        paymentForm.setAttribute("method", form.submitForm.method),
-                                        paymentForm.setAttribute("target", winRef.name),
-                                        paymentForm.innerHTML = form.submitForm.innerHTML,
-                                        document.documentElement.appendChild(paymentForm),
-                                        paymentForm.submit(),
-                                        document.documentElement.removeChild(paymentForm);
-                                    break;
+    try {
+        if (!event.data.isValidRequest) {
+            const motoResponse = event.data;
+            const paymentObj = getAppData('paymentObj');
+            if (event.origin === ("http://localhost") && motoResponse.redirectUrl) { //url check has to configured, currently its hardcoded
+                if (paymentObj.mode.toLowerCase() === "dropout") {
+                    singleHopDropOutFunction(motoResponse.redirectUrl);
+                }
+                else {
+                    // if(winRef && winRef.closed)
+                    // {
+                    //     //handlersMap["serverErrorHandler"](response);
+                    //     return;
+                    // }
+                    singleHopDropInFunction(motoResponse.redirectUrl).then(function (response) {
+                        if (winRef && winRef.closed !== true) {
+                            let el = document.createElement('body');
+                            el.innerHTML = response;
+                            let form = el.getElementsByTagName('form');
+                            try {
+                                let paymentForm = document.createElement('form');
+                                switch (Object.prototype.toString.call(form)) {
+                                    case "[object NodeList]" :
+                                        paymentForm.setAttribute("action", form[0].action),
+                                            paymentForm.setAttribute("method", form[0].method),
+                                            paymentForm.setAttribute("target", winRef.name),
+                                            paymentForm.innerHTML = form[0].innerHTML,
+                                            document.documentElement.appendChild(paymentForm),
+                                            paymentForm.submit(),
+                                            document.documentElement.removeChild(paymentForm);
+                                        break;
+                                    case "[object HTMLCollection]" :
+                                        paymentForm.setAttribute("action", form.submitForm.action),
+                                            paymentForm.setAttribute("method", form.submitForm.method),
+                                            paymentForm.setAttribute("target", winRef.name),
+                                            paymentForm.innerHTML = form.submitForm.innerHTML,
+                                            document.documentElement.appendChild(paymentForm),
+                                            paymentForm.submit(),
+                                            document.documentElement.removeChild(paymentForm);
+                                        break;
+                                }
+                            } catch (e) {
+                                console.log(e);
+                                let paymentForm = document.createElement('form');
+                                paymentForm.setAttribute("action", form.returnForm.action),
+                                    paymentForm.setAttribute("method", form.returnForm.method),
+                                    paymentForm.setAttribute("target", winRef.name),
+                                    paymentForm.innerHTML = form.returnForm.innerHTML,
+                                    document.body.appendChild(paymentForm),
+                                    paymentForm.submit(),
+                                    document.body.removeChild(paymentForm);
                             }
-                        } catch (e) {
-                            console.log(e);
-                            let paymentForm = document.createElement('form');
-                            paymentForm.setAttribute("action", form.returnForm.action),
-                                paymentForm.setAttribute("method", form.returnForm.method),
-                                paymentForm.setAttribute("target", winRef.name),
-                                paymentForm.innerHTML = form.returnForm.innerHTML,
-                                document.body.appendChild(paymentForm),
-                                paymentForm.submit(),
-                                document.body.removeChild(paymentForm);
                         }
-                    }
-                    if (!isIE()) {
-                        workFlowForModernBrowsers(winRef);
-                    } else {
-                        workFlowForIE(winRef);
-                    }
-                });
+                        if (!isIE()) {
+                            workFlowForModernBrowsers(winRef);
+                        } else {
+                            workFlowForIE(winRef);
+                        }
+                    });
+                }
+            } else {
+                if (winRef) {
+                    winRef.close();
+                }
+                const response = refineMotoResponse(motoResponse);
+                handlersMap['serverErrorHandler'](response);
             }
-        } else {
-            if (winRef) {
-                winRef.close();
-            }
-            const response = refineMotoResponse(motoResponse);
-            handlersMap['serverErrorHandler'](response);
+        } else if (event.data.isValidRequest && getAppData('paymentObj')['mode'].toLowerCase() === 'dropin') {
+            winRef = openPopupWindow("");
+            winRef.document.write('<html><head> <meta name="viewport" content="width=device-width"/> <meta http-equiv="Cache-control" content="public"/> <title>Redirecting to Bank</title></head><style>body{background: #fafafa;}#wrapper{position: fixed; position: absolute; top: 10%; left: 0; right: 0; margin: 0 auto; font-family: Tahoma, Geneva, sans-serif; color: #000; text-align: center; font-size: 14px; padding: 20px; max-width: 500px; width: 70%;}.maintext{font-family: Roboto, Tahoma, Geneva, sans-serif; color: #f6931e; margin-bottom: 0; text-align: center; font-size: 16pt; font-weight: 400;}.textRedirect{color: #675f58;}.subtext{margin: 15px 0 15px; font-family: Roboto, Tahoma, Geneva, sans-serif; color: #929292; text-align: center; font-size: 10pt;}.subtextOne{margin: 35px 0 15px; font-family: Roboto, Tahoma, Geneva, sans-serif; color: #929292; text-align: center; font-size: 10pt;}@media screen and (max-width: 480px){#wrapper{max-width: 100%!important;}}</style><body> <div id="wrapper"> <div id="imgtext" style="margin-left:1%; margin-bottom: 5px;"><!--<img src="https://context.citruspay.com/kiwi/images/logo.png"/>--> </div><div id="imgtext" style="text-align:center;padding: 15% 0 10%;"><!---<img src="https://context.citruspay.com/kiwi/images/puff_orange.svg"/>--></div><p class="maintext">Processing <span class="textRedirect">Payment</span> </p><p class="subtext"><span>We are redirecting you to the bank\'s page</span></p><p class="subtextOne"><span>DO NOT CLOSE THIS POP-UP</span> </p></div></body></html>');
         }
-     }else if(event.data.isValidRequest && getAppData('paymentObj')['mode'].toLowerCase() === 'dropin') {
-         winRef = openPopupWindow("");
-         winRef.document.write('<html><head> <meta name="viewport" content="width=device-width"/> <meta http-equiv="Cache-control" content="public"/> <title>Redirecting to Bank</title></head><style>body{background: #fafafa;}#wrapper{position: fixed; position: absolute; top: 10%; left: 0; right: 0; margin: 0 auto; font-family: Tahoma, Geneva, sans-serif; color: #000; text-align: center; font-size: 14px; padding: 20px; max-width: 500px; width: 70%;}.maintext{font-family: Roboto, Tahoma, Geneva, sans-serif; color: #f6931e; margin-bottom: 0; text-align: center; font-size: 16pt; font-weight: 400;}.textRedirect{color: #675f58;}.subtext{margin: 15px 0 15px; font-family: Roboto, Tahoma, Geneva, sans-serif; color: #929292; text-align: center; font-size: 10pt;}.subtextOne{margin: 35px 0 15px; font-family: Roboto, Tahoma, Geneva, sans-serif; color: #929292; text-align: center; font-size: 10pt;}@media screen and (max-width: 480px){#wrapper{max-width: 100%!important;}}</style><body> <div id="wrapper"> <div id="imgtext" style="margin-left:1%; margin-bottom: 5px;"><!--<img src="https://context.citruspay.com/kiwi/images/logo.png"/>--> </div><div id="imgtext" style="text-align:center;padding: 15% 0 10%;"><!---<img src="https://context.citruspay.com/kiwi/images/puff_orange.svg"/>--></div><p class="maintext">Processing <span class="textRedirect">Payment</span> </p><p class="subtext"><span>We are redirecting you to the bank\'s page</span></p><p class="subtextOne"><span>DO NOT CLOSE THIS POP-UP</span> </p></div></body></html>');
+        if (event.data.responseType === "errorHandler") handlersMap['errorHandler'](event.data.error);
+        if (event.data.responseType === "serverErrorHandler") handlersMap['serverErrorHandler'](event.data.error);
     }
-    if(event.data.responseType === "errorHandler") handlersMap['errorHandler'](event.data.error);
-    if(event.data.responseType === "serverErrorHandler") handlersMap['serverErrorHandler'](event.data.error);
-    }
-    catch(e){
+    catch (e) {
         console.log(e);
     }
 }
