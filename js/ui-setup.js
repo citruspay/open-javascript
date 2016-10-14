97f8bdf926c112fa2c1c745cc97a2fcf58fe5e41
Rakesh Pathak <rakesh@talentica-all.com>
2016-10-14 13:41:40 +0530

    supported placeHolder attribute in hosted fields.

--- js/ui-setup.js
+++ js/ui-setup.js
@@ -20,7 +20,7 @@ const uiSetup = (setUpConfig) => {
             identifier
         } = hostedFields[i];
         if (validHostedFieldTypes.indexOf(fieldType) !== -1) {
-            appendIframe(identifier, fieldType, cardType.toLowerCase(), style);
+            appendIframe(hostedFields[i], cardType.toLowerCase(), style);
         } else {
             throw new Error(`invalid fieldType "${fieldType}", fieldType should have one of these values ` + validHostedFieldTypes);
         }
@@ -29,36 +29,45 @@ const uiSetup = (setUpConfig) => {
 };
 
 
-const appendIframe = (identifier, fieldType, cardType, style) => {
+const appendIframe = (hostedField, cardType, style) => {
     "use strict";
+    let {identifier,fieldType} = hostedField;
     const invalidIdentifierMessage = `invalid identifier for field type "${fieldType}", it should be of the form of #id or .cssClass`;
     const iframe = document.createElement('iframe');
     var defaultStyle = {
         width: '100%',
         float: 'left',
-        height: '52px',
-        'marginBottom': '1em',
+        height: '100%',
+        /*'marginBottom': '1em',
         'display': 'block',
-        backgroundColor: 'transparent',
+        backgroundColor: 'transparent',*/
         border: 'none',
-        outline: '0',
+        /*outline: '0',
         fontSize: '16px',
         padding: 0,
         boxShadow: 'none',
         borderRadius: 0,
-        position: 'relative'
+        position: 'relative'*/
     };
     //frameborder="0" allowtransparency="true" scrolling="no"
     iframe.setAttribute('frameborder', 0);
     iframe.setAttribute('allowtransparency', true);
     iframe.setAttribute('scrolling', 'no');
+    //iframe.setAttribute('tabindex', '-1');
     Object.assign(iframe.style, defaultStyle);
     //todo: url needs to be configured
     iframe.src = getConfigValue('hostedFieldUrl') + '#' + fieldType + '-' + cardType;
     iframe.id = getCitrusFrameId(fieldType, cardType);
     iframe.onload = () => {
-        setStyle(style, identifier, fieldType, cardType);
-    }
+        console.log('inside iframe onload');
+        setStyle(style, hostedField, cardType); 
+    };
+    iframe.onfocus = ()=>{
+        console.log('inside iframe onfocus');
+        var inputElements = document.getElementsByTagName('input');
+        if(inputElements&&inputElements.length>0)
+        inputElements[0].focus();
+    };
     if (!identifier || identifier.length <= 1)
         throw new Error(invalidIdentifierMessage);
     const identifierName = identifier.slice(1);
@@ -68,24 +77,26 @@ const appendIframe = (identifier, fieldType, cardType, style) => {
         document.getElementsByClassName(identifierName)[0].appendChild(iframe);
     else throw new Error(invalidIdentifierMessage);
 }
-
-const setStyle = (style, identifier, fieldType, cardType) => {
-    if (!style)
-        return;
+//todo:rename to setStyle and other attributes
+const setStyle = (style, hostedField, cardType) => {
     let hostedFrames = [];
     //for(var i=0;i<hostedFields.length;++i)
     //{
-    let hostedFrameStyle = {
+    let hostedFrameAttributes = {
         messageType: 'style'
     };
-    //let {identifier,fieldType} = hostedField;
+    let {identifier,fieldType} = hostedField;
+    if(style)
+    {
     if (style[identifier]) {
-        hostedFrameStyle.specificStyle = style[identifier];
+        hostedFrameAttributes.specificStyle = style[identifier];
     }
     if (style['input']) {
-        hostedFrameStyle.commonStyle = style['input'];
+        hostedFrameAttributes.commonStyle = style['input'];
     }
-    postMessageToChild(fieldType, cardType, hostedFrameStyle, true);
+    }
+    hostedFrameAttributes.hostedField = hostedField;
+    postMessageToChild(fieldType, cardType, hostedFrameAttributes, true);
     //}
 }
 
@@ -110,6 +121,10 @@ const applyStyle = (style) => {
     createSytleObject(style.commonStyle);
     createSytleObject(style.specificStyle);
     var inputElement = document.getElementsByTagName('input')[0];
+    if(style.hostedField&&style.hostedField.placeHolder)
+    {
+        inputElement.setAttribute('placeholder',style.hostedField.placeHolder);
+    }
     Object.assign(inputElement.style, applicableStyle);
 
 }
