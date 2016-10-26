51a80a6fd57eba4905de61f2a43f454ad88af4da
Rakesh Pathak <rakesh@talentica-all.com>
2016-10-26 16:07:38 +0530

    renamed three files

--- js/hosted-field-config.js
+++ js/hosted-field-config.js
@@ -1,5 +1,5 @@
-const HOSTED_FIELD_DOMAIN = 'http://localhost'
-const CREDIT_CARD_GENERATOR_URL =  HOSTED_FIELD_DOMAIN + '/cards.php';
+const HOSTED_FIELD_DOMAIN = 'http://localhost:8085'
+const CREDIT_CARD_GENERATOR_URL =  HOSTED_FIELD_DOMAIN + '/cards.html';
 const getConfigValue = (key) =>{
     "use strict";
     switch(key){
