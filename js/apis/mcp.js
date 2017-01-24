30de2a3fe70a2d7747fbb3b43e24766c2431f1e4
nagamaCitruspay <nagama.inamdar@citruspay.com>
2017-01-24 12:43:35 +0530

    changes for mcp

--- js/apis/mcp.js
+++ js/apis/mcp.js
@@ -1,20 +1,20 @@
-import {validateAndCallbackify} from "./../utils";
+import {validateAndCallbackify, schemeFromNumber} from "./../utils";
 import {validateScheme} from "../validation/custom-validations";
 import {custFetch} from "../interceptor";
 import {motoCardValidationSchema, motoCardApiFunc} from "./cards";
 import {MCPData} from "./payment-details";
 import cloneDeep from "lodash/cloneDeep";
-import {currencyMap} from "../constants";
+//import {currencyMap} from "../constants";
 
 const MCPCardSchema = cloneDeep(motoCardValidationSchema);
 
-const countryCurrencyMap =  currencyMap;
+//const countryCurrencyMap = currencyMap;
 
 //MCPCardSchema.mainObjectCheck.keysCheck.push('targetMcpCurrency');
 
 const makeMCPCardPayment = validateAndCallbackify(MCPCardSchema, (confObj) => {
     MCPData.MCPWrapperAPIData.mcpConversionBeans.some((el) => {
-        if(el.targetCurrency === confObj.targetMcpCurrency){
+        if (el.targetCurrency === confObj.targetMcpCurrency) {
             confObj.currencyToken = el.token;
             return true;
         }
@@ -27,23 +27,23 @@ const binServiceSchema = {
 };
 
 const getCardCurrencyInfo = validateAndCallbackify(binServiceSchema, (confObj) => {
-    const sixDigitCardNum = ''+confObj.cardNumber.substr(0,6);
+    const sixDigitCardNum = '' + confObj.cardNumber.substr(0, 6);
+    let aliasedScheme = validateScheme(schemeFromNumber(confObj.cardNumber), false);
     return custFetch(`https://bin.citruspay.com/binservice/v1/bin/${sixDigitCardNum}`, {
         method: 'get'
-    }).then((resp)=>{
-        let aliasedScheme;
-        if(resp.data.cardscheme){
-            aliasedScheme = validateScheme(resp.data.cardscheme);
-            if(!aliasedScheme){
+    }).then((resp)=> {
+        if (resp.data.cardscheme) {
+            aliasedScheme = validateScheme(resp.data.cardscheme, false);
+            if (!aliasedScheme) {
                 throw 'scheme mapping not found!';
             }
-        }else{
+        } else if (!aliasedScheme) {
             throw 'Not valid card!'
         }
         let cardCurrency;
         resp.data.currency_code ? cardCurrency = resp.data.currency_code : cardCurrency = "";
 
-        if(MCPData.calculatedMCPSchemes.indexOf(aliasedScheme) > -1){
+        if (MCPData.calculatedMCPSchemes.indexOf(aliasedScheme) > -1) {
             if (cardCurrency === MCPData.baseCurrency) {
                 return [];
             } else if (!cardCurrency) {
@@ -53,15 +53,16 @@ const getCardCurrencyInfo = validateAndCallbackify(binServiceSchema, (confObj) =
                         amount: el.amount
                     }
                 });
-            }else{
-                return MCPData.MCPWrapperAPIData.mcpConversionBeans.map((el)=>{
-                    return {
-                        currency: el.targetCurrency,
-                        amount: el.amount
-                    }
-                });
             }
-        }else{
+            let mcpCurrency = [];
+            MCPData.MCPWrapperAPIData.mcpConversionBeans.some((el)=> {
+                if (cardCurrency === el.targetCurrency) {
+                     mcpCurrency = [{currency: el.targetCurrency, amount: el.amount}];
+                    return true;
+                }
+            });
+            return mcpCurrency;
+        } else {
             return [];
         }
     });
