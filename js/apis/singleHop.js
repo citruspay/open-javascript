cf575cf84173c3e46e829d9275cf3dc358c1e3dd
nagamaCitruspay <nagama.inamdar@citruspay.com>
2017-03-10 20:18:36 +0530

    added change for iframe solution of v3

--- js/apis/singleHop.js
+++ js/apis/singleHop.js
@@ -1,18 +1,10 @@
 import {custFetch} from '../interceptor';
 
 const singleHopDropOutFunction = (url) => {
-
     return custFetch(url, {
         method: 'get',
         mode: 'cors'
     }).then(function (response) {
-
-        /* OL integration logic to be uncommented later*/
-        // document.open('text/html');
-        // document.write(response);
-        // document.close();
-        // return;
-        /*End of OL integration logic*/
         let el = document.createElement('body');
         el.innerHTML = response.data;
         let form = el.getElementsByTagName('form');
@@ -20,6 +12,7 @@ const singleHopDropOutFunction = (url) => {
             let paymentForm = document.createElement('form');
             paymentForm.setAttribute("action", form[0].action);
             paymentForm.setAttribute("method", form[0].method);
+            paymentForm.setAttribute("target", '_parent');
             paymentForm.innerHTML = form[0].innerHTML;
             document.documentElement.appendChild(paymentForm);
             paymentForm.submit();
